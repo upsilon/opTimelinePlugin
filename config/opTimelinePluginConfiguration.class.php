1b698cd2868c223ce0a363a8399192471ba4eee8
Kimura Youichi <kim.upsilon@bucyou.net>
2018-02-14 21:55:52 +0900

    Composerパッケージの設定を追加

---
+++ config/opTimelinePluginConfiguration.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class opTimelinePluginConfiguration extends sfPluginConfiguration
+{
+  public function initialize()
+  {
+    require_once $this->getRootDir().'/vendor/autoload.php';
+  }
+}
