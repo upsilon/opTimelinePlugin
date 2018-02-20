b51173a5f2516e23b0d3bed073bb452156368bad
Kimura Youichi <kim.upsilon@bucyou.net>
2018-02-21 01:54:50 +0900

    eslintの設定を追加

--- assets/js/packs/timeline-smt.js
+++ assets/js/packs/timeline-smt.js
@@ -3,6 +3,7 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import TimelineSmt from '../apps/TimelineSmt.vue';
 
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   render: h => h(TimelineSmt),
